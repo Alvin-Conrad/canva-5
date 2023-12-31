38f2de0976fea81fa3015d5b378f414a868a1d79
Francis.dev <149237151+FrancisGil-dev@users.noreply.github.com>
2023-12-31 19:48:40 +0800

    commit

--- screen/Intro.js
+++ screen/Intro.js
@@ -3,13 +3,14 @@ import { View, Text, Pressable, ImageBackground } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { useNavigation } from '@react-navigation/native'
 import Tutorial from './Tutorial'
+import { MaterialIcons } from '@expo/vector-icons'; 
+
+
 const Intro = () => {
   const image = { uri: 'https://i.pinimg.com/564x/59/f7/e3/59f7e3c963993ca2ce195b045fc7657f.jpg' };
   const navigation = useNavigation();
 
   return (
-   
-
     <View style={{flex: 1}}>
     {/*BG image*/}
     <ImageBackground source={image} resizeMode="cover" style={{flex: 1, justifyContent: 'center'}}>
@@ -19,8 +20,8 @@ const Intro = () => {
           <Text style={{width: '70%', fontSize: 20,marginBottom:20}}>Welcome to the Canva Guide App, your personalized tutor for mastering the art of design with Canva. This app is designed especially for beginners, providing step-by-step tutorials to help you navigate Canva with ease.</Text>
      
             {/*BTN*/}
-          <Pressable style={{backgroundColor: 'black',width: 145, padding: 3,position: 'absolute', right: 20,top: 350 }} onPress={() => navigation.navigate('Tutorial')}>
-            <Text style={{color: 'white', fontSize: 24, borderRadius: 50, textAlign: 'center'}}>Let's Start</Text>
+          <Pressable style={{backgroundColor: 'black',width: 145, padding: 5,position: 'absolute', right: 20,top: 350,}} onPress={() => navigation.navigate('Tutorial')}>
+            <Text style={{color: 'white', fontSize: 20, borderRadius: 50, textAlign: 'center'}}>Let's Start  <MaterialIcons name="double-arrow" size={20} color="white"/></Text>
           </Pressable>
         </View>
       </ImageBackground>
--- screen/Tutorial.js
+++ screen/Tutorial.js
@@ -1,16 +1,41 @@
+
 import React from 'react'
-import { Text, View } from 'react-native';
-import VideoList from './component/VideoList.js'
+import { Feather } from '@expo/vector-icons';
+import { Pressable, Text, View } from 'react-native';
+
 
 
 const Tutorial = () => {
-
-
-
   return(
-    <View >
-      <VideoList />
-    </View>
+   
+    <View style={{flex :1,}}>
+    
+     {/*Header*/}
+     <View style={{width: '100%', height: 150 ,justifyContent: 'center', alignItems: 'center',}}>
+     
+        {/*Navbar Container*/}
+        <View style={{display: 'flex', flexDirection: 'row', gap: 200,alignItems: 'center',}}>
+          <View>
+            <Text style={{fontSize: 30}}><Text style={{color: '#8cb2ff'}}>CANVA</Text> GUIDE APP</Text>
+          </View>
+
+          {/*Toggle Dark Mode*/}
+          <View>
+            <Pressable onPress={() => alert('Toggle Dark')}>
+              <Feather name="moon" size={24} color="black" />
+            </Pressable>
+          </View>
+        </View>
+        {/*Vertical Line*/}
+        <View style={{height: 1,width: '100%',backgroundColor: '#737373',position: 'relative', top: 30 }}/>
+        
+     </View>
+      {/*Main Tutorial*/}
+     <View>
+
+     </View>      
+   </View>
+   
  )
 }
 
